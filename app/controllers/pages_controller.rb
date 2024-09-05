8dfb554ec1d934e8b814b42f0bf14885c00005bf
Ali Aizad <aliaizad72@gmail.com>
2024-09-05 11:09:39 +0800

    Complete the page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
-  @flickr = Flickr.new
   def index
-
+    @flickr = Flickr.new
+    if params[:username]
+      begin
+        @user = @flickr.people.findByUsername(username: params[:username])
+        @photos_list = @flickr.people.getPublicPhotos(user_id: @user["id"])
+      rescue Flickr::FailedResponse
+        flash[:alert] = "No such username found"
+        redirect_to root_path
+      end
+    end
   end
 end
